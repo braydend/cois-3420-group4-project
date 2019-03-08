2b2c20279f33ee924b812273aed4fbd44dab537b
Unknown <braydendiluca@gmail.com>
2019-03-08 11:44:28 -0500

    [WIP] Created header.php
    Need to decide what menu items should be displayed to the user, how to manage the breadcrumbs and basic CSS has been added

---
+++ header.php
@@ -0,0 +1,45 @@
+<head>
+    <title>
+        <?PHP echo($title) ?>
+    </title>
+    <link rel="stylesheet" href="css/basestyle.css" />
+</head>
+<body>
+    <nav id="page-navigation">
+        <?PHP
+            session_start();
+
+            // Menu to be displayed to authed users
+            function echoNav(){
+                // Need to decide what to put in nav
+                echo("<li>Nav Item 1</li>");
+                echo("<li>Nav Item 2</li>");
+                echo("<li>Nav Item 3</li>");
+                echo("<li><a href='logout.php'>Logout</a></li>");
+            }
+
+            // Menu to be displayed to unauthed users
+            function echoLogInMenu(){
+                echo("<li><a href='login.php'>Login</a></li>");
+                echo("<li><a href='register.php'>Register</a></li>");
+            }
+
+            // To display to the users where they are on the site
+            function echoBreadcrumbs(){
+                // Need to add logic to change this dynamically
+                // Maybe use $title?
+                echo("<span>You are here</span>");
+            }
+
+            // Logic controlling which menu to display based
+            // on whether or not the user is logged in
+            echoBreadcrumbs();
+            echo("<ul>");
+            if(isset($_SESSION['userid'])){
+                echoNav();
+            }else{
+                echoLogInMenu();
+            }
+            echo("</ul>");
+        ?>
+    </nav>
