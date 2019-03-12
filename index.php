fe4290c7ed5682ce8b113041f35c27a88b47ca12
Unknown <braydendiluca@gmail.com>
2019-03-12 14:14:47 -0400

    [WIP] temporary index.php fro testing header.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?PHP
+    $title = "Homepage";
+    include('header.php');
+?>
+    <h1>Index for testing</h1>
+<?PHP
+    // For testing the menu functionality
+    $_SESSION['userid'] = true;
+?>
+</body>
+</html>
