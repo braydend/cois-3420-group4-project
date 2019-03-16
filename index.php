c02c918ad2872264758605e9573a301900ea5a19
Unknown <braydendiluca@gmail.com>
2019-03-16 18:42:19 -0400

    Fixed merge issues

--- index.php
+++ index.php
@@ -3,57 +3,6 @@
     include('header.php');
     // For testing the menu functionality
     $_SESSION['userid'] = true;
-add_header.php
-?>
-    <div class="container">
-        <h1>Movies</h1>
-        <div class="movie-grid">
-            <div class="movie">
-                <div class="movie-img">
-                    <img src="images/dummy_movie.jpg" alt="Movie Image" />
-                </div>
-                <div class="movie-btns">
-                    <a href="#"><i class="fas fa-pencil-alt"></i></a>
-                    <a href="#"><i class="fas fa-trash-alt"></i></a>
-                    <a href="#"><i class="fas fa-info-circle"></i></a>
-                </div>
-            </div>
-
-            <div class="movie">
-                <div class="movie-img">
-                    <img src="images/dummy_movie.jpg" alt="Movie Image" />
-                </div>
-                <div class="movie-btns">
-                    <a href="#"><i class="fas fa-pencil-alt"></i></a>
-                    <a href="#"><i class="fas fa-trash-alt"></i></a>
-                    <a href="#"><i class="fas fa-info-circle"></i></a>
-                </div>
-            </div>
-
-            <div class="movie">
-                <div class="movie-img">
-                    <img src="images/dummy_movie.jpg" alt="Movie Image" />
-                </div>
-                <div class="movie-btns">
-                    <a href="#"><i class="fas fa-pencil-alt"></i></a>
-                    <a href="#"><i class="fas fa-trash-alt"></i></a>
-                    <a href="#"><i class="fas fa-info-circle"></i></a>
-                </div>
-            </div>
-
-            <div class="movie">
-                <div class="movie-img">
-                    <img src="images/dummy_movie.jpg" alt="Movie Image" />
-                </div>
-                <div class="movie-btns">
-                    <a href="#"><i class="fas fa-pencil-alt"></i></a>
-                    <a href="#"><i class="fas fa-trash-alt"></i></a>
-                    <a href="#"><i class="fas fa-info-circle"></i></a>
-                </div>
-            </div>
-        </div>
-    </div>
-
 
     // For accessing DB
     include('includes/library.php');
