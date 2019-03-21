8dd8bb162ce8c9bc2ca7a4a1b6f6ef675a37fb36
Joshua <joshuacroft@trentu.ca>
2019-03-21 18:23:12 -0400

    - Changed the redirect to the login page instead of the search page on first load when not logged in(If the user is not logged in and wants to edit account, it makes sense that they should be prompted to login so that they can edit). - a lot of Commenting / formatting. - changed the if-elseif-elseif-etc statements in the foreach loop for the checkboxes to a switch statement. - Added flags in the upper portion of the code (php) so as to allow dynamically printing content to the body of the html below. - Moved the initial header down into the body and out of the php condition checking for if the user is logged in. We redirect right away when not logged in so no point in leaving that up there. - Changed some conditions around to fix up some logic. - Started adding validation.

--- editAccount.php
+++ editAccount.php
@@ -1,53 +1,115 @@
 <?php
+
 	$title = "Account Details";
+
 	include('includes/library.php');
-    include('header.php');
+  include('header.php');
+
+	// Flag variables.
+	$boxesChecked = true;
+	$validUsername = true;
+	$usernameFree = true;
+	$validPassword = true;
+	$validName = true;
+	$passwordMatchesConfirm = true;
+	$validEmail = true;
+	$emailFree = true;
+
+	// Get db connection.
 	$pdo = dbconnect();
-	
-	// sends back to home if not logged in, 
-	if (isset($_SESSION['userid']))
-	{
-		echo "<h3>Check the boxes for the fields you wish to update</h3>";
-	}
-	else
-	{
-		header("location:search.php");
+
+
+	// --- User is not logged in on page load. Send user to the login page.
+	if ( !isset($_SESSION['userid']) ) {
+
+    // Redirect to login.
+		header("location:login.php");
 		exit();
+
 	}
-	
-	if(!empty($_POST['check']))
-	{
-		// checks each of the checkboxes on the main form and runs the queries if they are checked (0 validation done, stuff is super easy to break)
-		foreach($_POST['check'] as $check)
-		{
-			if($check == 'username')
-			{
-				$sql = "UPDATE user_accounts SET username = ? WHERE user_id = ?";
-				$pdo->prepare($sql)->execute([$_POST['username'], $_SESSION['userid']]);
 
-				header('location:index.php');
-			}	
-			else if($check == 'name')
-			{
-				$sql = "UPDATE user_accounts SET name = ? WHERE user_id = ?";
-				$pdo->prepare($sql)->execute([$_POST['name'], $_SESSION['userid']]);
-				
-				header('location:index.php');
-			}
-			else if($check == 'email')
-			{
-				$sql = "UPDATE user_accounts SET email = ? WHERE user_id = ?";
-                $pdo->prepare($sql)->execute([$_POST['email'], $_SESSION['userid']]);
+	// --- If past this point user is logged in and has userid in session variable.
 
-                header('location:index.php');
+  // No checkboxes selected on submit.
+	if( !isset($_POST['check']) && isset( $_POST['submit']) ) {
+
+		// Set flag to tell the user to check atleast one box to modify the account.
+		$boxesChecked = false;
+
+	}
+
+  // Atleast one checkbox was selected on submit.
+  else if ( isset( $_POST['submit']) ) ){
+
+		// checks each of the checkboxes on the main form and runs the queries if they are checked.
+		foreach($_POST['check'] as $check) {
+
+      // Validation is identical to add-account.php
+			switch($check) {
+
+				case 'username':
+
+					if ()
+
+					//$sql = "UPDATE user_accounts SET username = ? WHERE user_id = ?";
+					//$pdo->prepare($sql)->execute([$_POST['username'], $_SESSION['userid']]);
+
+					echo "You checked username!";
+
+					break;
+
+				case 'name':
+
+					//$sql = "UPDATE user_accounts SET name = ? WHERE user_id = ?";
+					//$pdo->prepare($sql)->execute([$_POST['name'], $_SESSION['userid']]);
+
+					echo "You checked name!";
+
+					break;
+
+				case 'email':
+
+					//$sql = "UPDATE user_accounts SET email = ? WHERE user_id = ?";
+					//$pdo->prepare($sql)->execute([$_POST['email'], $_SESSION['userid']]);
+
+					echo "You checked email!";
+
+					break;
+
+				case 'password':
+
+					// Validate Password
+					// Inspired by: https://stackoverflow.com/questions/22544250/php-password-validation
+					if (strlen($_POST["password"]) < '8') {
+						$validPassword = false;
+						$passErr = "Your Password Must Contain At Least 8 Characters";
+					}
+					elseif( !preg_match("#[0-9]+#", $_POST["password"]) ) {
+						$validPassword = false;
+						$passErr = "Your Password Must Contain At Least 1 Number";
+					}
+					elseif( !preg_match("#[A-Z]+#", $_POST["password"]) ) {
+						$validPassword = false;
+						$passErr = "Your Password Must Contain At Least 1 Capital Letter";
+					}
+					elseif( !preg_match("#[a-z]+#", $_POST["password"]) ) {
+						$validPassword = false;
+						$passErr = "Your Password Must Contain At Least 1 Lowercase Letter";
+					}
+
+					// Validate Password Confirm (equals password)
+					if ( !( $_POST['password'] === $_POST['password_confirm'] ) ) {
+						$passwordMatchesConfirm = false;
+					}
+
+					break;
+
 			}
-			else if($check =='password')
-			{	
-				
-			}
+
+			// header('location:index.php'); // This was after each query.
 		}
 	}
-	
+
 ?>
 
 <!DOCTYPE HTML>
@@ -59,6 +121,7 @@
 <body>
     <div class="container">
         <h1>Modify an account:</h1>
+				<h3>Check the boxes for the fields you wish to update</h3>
         <form method='post' class='container' name ='details'>
             <div class="form-element">
                 <label for="username">Username:</label>
@@ -69,23 +132,23 @@
             <div class="form-element">
                 <label for="name">Name:</label>
                 <input type="text" name="name" id="name" />
-				<input type="checkbox" name="check[]" value='name'/>
+								<input type="checkbox" name="check[]" value='name'/>
            </div>
             <div class="form-element">
                 <label for="email">Email:</label>
                 <input type="email" name="email" id="email" />
-				<input type="checkbox" name="check[]" value='email'/>
+								<input type="checkbox" name="check[]" value='email'/>
            </div>
             <div class="form-element">
                 <label for="password">Password:</label>
                 <input type="password" name="password" id="password" />
-				<input type="checkbox" name="check[]" value='password'/>
+								<input type="checkbox" name="check[]" value='password'/>
            </div>
             <div class="form-element">
                 <label for="password_confirm">Confirm Password:</label>
                 <input type="password" name="password_confirm" id="password_confirm" />
             </div>
-			
+
 			<div class="form-buttons">
                 <input type="submit" value="Update Account!" />
                 <input type="reset" />
