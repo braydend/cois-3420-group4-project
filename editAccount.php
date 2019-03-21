e0c3c7b8c5abf5817094f3c457482623a89874c3
Joshua <joshuacroft@trentu.ca>
2019-03-21 19:23:42 -0400

    - More validation - a lot more commenting - moved the queries to another switch case below the validation switch case. - Added inline php in html for printing error messages when input is not valid (based on flags set in above php). Identical to the acount.php page. - cleaned up indenting in html - Added redirect to index after successful update of account. - Added password hash before Updating table with new password.

--- editAccount.php
+++ editAccount.php
@@ -19,6 +19,7 @@
 	$pdo = dbconnect();
 
 
+
 	// --- User is not logged in on page load. Send user to the login page.
 	if ( !isset($_SESSION['userid']) ) {
 
@@ -28,9 +29,13 @@
 
 	}
 
+
+
 	// --- If past this point user is logged in and has userid in session variable.
 
-  // No checkboxes selected on submit.
+
+
+  // --- No checkboxes selected on submit.
 	if( !isset($_POST['check']) && isset( $_POST['submit']) ) {
 
 		// Set flag to tell the user to check atleast one box to modify the account.
@@ -38,44 +43,75 @@
 
 	}
 
-  // Atleast one checkbox was selected on submit.
-  else if ( isset( $_POST['submit']) ) ){
 
-		// checks each of the checkboxes on the main form and runs the queries if they are checked.
-		foreach($_POST['check'] as $check) {
+
+  // --- Atleast one checkbox was selected on submit.
+  else if ( isset($_POST['submit'])) ) {
+
+
+		// --- VALIDATE each selected field in the form.
+		foreach( $_POST['check'] as $check ) {
 
       // Validation is identical to add-account.php
-			switch($check) {
+			switch( $check ) {
 
+        // username validation.
 				case 'username':
 
-					if ()
+					// Enusre that username given does not already exist in the table ...
+			    $query = "SELECT 1 username FROM user_accounts WHERE username = ?";
+			    $stmt = $pdo->prepare($query);
+			    $stmt->execute([$_POST['username']]);
+			    $result = $stmt->fetchAll();
 
-					//$sql = "UPDATE user_accounts SET username = ? WHERE user_id = ?";
-					//$pdo->prepare($sql)->execute([$_POST['username'], $_SESSION['userid']]);
+			    // There already exists an account with given username.
+			    if (!empty($result)) {
 
-					echo "You checked username!";
+			      // Flag as invalid.
+			      $usernameFree = false;
+			    }
+
+					// Validate Username (between 3 and 20 characters inclusive) (only regular chars and numbers allowed)
+					if (!( strlen($_POST['username']) >= 3 && strlen($_POST['username']) <= 25 && preg_match("/^[a-zA-Z0-9]*$/", $_POST['username']) )) {
+						$validUsername = false;
+					}
 
 					break;
 
+        // name validation.
 				case 'name':
 
-					//$sql = "UPDATE user_accounts SET name = ? WHERE user_id = ?";
-					//$pdo->prepare($sql)->execute([$_POST['name'], $_SESSION['userid']]);
-
-					echo "You checked name!";
+					// Validate Name (between 3 and 40 characters inclusive) (only chars and white space allowed)
+			    if (!( strlen($_POST['name']) >= 2 && strlen($_POST['name']) <= 40 && preg_match("/^[a-zA-Z ]*$/", $_POST['name']) )) {
+			      $validName = false;
+			    }
 
 					break;
 
+        // email validation.
 				case 'email':
 
-					//$sql = "UPDATE user_accounts SET email = ? WHERE user_id = ?";
-					//$pdo->prepare($sql)->execute([$_POST['email'], $_SESSION['userid']]);
+					// Ensure that email given does not already exist in the table ...
+			    $query = "SELECT 1 email FROM user_accounts WHERE email = ?";
+			    $stmt = $pdo->prepare($query);
+			    $stmt->execute([$_POST['email']]);
+			    $result = $stmt->fetchAll();
 
-					echo "You checked email!";
+			    // There already exists an account with given username.
+			    if (!empty($result)) {
+
+			      // Flag as invalid.
+			      $emailFree = false;
+			    }
+
+					// Validate Email
+					if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+						$validEmail = false;
+					}
 
 					break;
 
+        // password validation.
 				case 'password':
 
 					// Validate Password
@@ -102,58 +138,142 @@
 						$passwordMatchesConfirm = false;
 					}
 
-					break;
+					break; // End password validation.
 
-			}
+			} // End switch for validation.
 
-			// header('location:index.php'); // This was after each query.
-		}
-	}
+		} // End foreach
+
+
+
+		// --- All input is valid. UPDATE all selected fields.
+		if ($validUsername && $validPassword && $validName && $validEmail && $passwordMatchesConfirm && $emailFree && $usernameFree) {
+
+			// --- UPDATE each selected field in the form.
+			foreach( $_POST['check'] as $check ) {
+
+				// Validation is identical to add-account.php
+				switch( $check ) {
+
+					// username UPDATE.
+					case 'username':
+
+						// UPDATE username in the users table.
+						$sql = "UPDATE user_accounts SET username = ? WHERE user_id = ?";
+						$pdo->prepare($sql)->execute([$_POST['username'], $_SESSION['userid']]);
+
+						break;	// End UPDATE username query.
+
+					// name UPDATE.
+					case 'name':
+
+						// UPDATE name in the users table.
+						$sql = "UPDATE user_accounts SET name = ? WHERE user_id = ?";
+						$pdo->prepare($sql)->execute([$_POST['name'], $_SESSION['userid']]);
+
+						break;	// End UPDATE name query.
+
+					// email UPDATE.
+					case 'email':
+
+						// UPDATE email in the users table.
+						$sql = "UPDATE user_accounts SET email = ? WHERE user_id = ?";
+						$pdo->prepare($sql)->execute([$_POST['email'], $_SESSION['userid']]);
+
+						break;	// End UPDATE email query.
+
+					// password UPDATE.
+					case 'password':
+
+						// Hash Password
+						$hash = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+						// UPDATE password in the users table.
+						$sql = "UPDATE user_accounts SET passsword = ? WHERE user_id = ?";
+						$pdo->prepare($sql)->execute([$hash, $_SESSION['userid']]);
+
+						break;  // End UPDATE password query.
+
+				} // End switch for slected checkbox to query.
+
+			} // End foreach selected checkbox.
+
+
+
+			// --- Redirect to index after updating account.
+			header("location:index.php");
+			exit();
+
+
+
+		} // End if (all input is valid) (and atleast one checkbox was selected).
+
+	} // End submit with atleast one checkbox checked.
 
 ?>
 
 <!DOCTYPE HTML>
 <html lang="en">
 <head>
-    <title>Create an Account</title>
-    <link rel="stylesheet" href="css/basestyle.css" />
+  <title>Create an Account</title>
+  <link rel="stylesheet" href="css/basestyle.css" />
 </head>
 <body>
-    <div class="container">
-        <h1>Modify an account:</h1>
-				<h3>Check the boxes for the fields you wish to update</h3>
-        <form method='post' class='container' name ='details'>
-            <div class="form-element">
-                <label for="username">Username:</label>
-                <input type="text" name="username" id="username" />
+  <div class="container">
+    <h1>Modify an account:</h1>
+		<h3>Check the boxes for the fields you wish to update</h3>
+		<?php if (isset($boxesChecked) && !$boxesChecked) {
+			echo "<span class='error'>Must select a field below to modify account.</span>";
+		} ?>
+    <form method='post' class='container' name ='details'>
+      <div class="form-element">
+        <label for="username">Username:</label>
+        <input type="text" name="username" id="username" />
 				<input type="checkbox" name="check[]" value='username' />
-            </div>
-			<!-- four mana seven seven -->
-            <div class="form-element">
-                <label for="name">Name:</label>
-                <input type="text" name="name" id="name" />
-								<input type="checkbox" name="check[]" value='name'/>
-           </div>
-            <div class="form-element">
-                <label for="email">Email:</label>
-                <input type="email" name="email" id="email" />
-								<input type="checkbox" name="check[]" value='email'/>
-           </div>
-            <div class="form-element">
-                <label for="password">Password:</label>
-                <input type="password" name="password" id="password" />
-								<input type="checkbox" name="check[]" value='password'/>
-           </div>
-            <div class="form-element">
-                <label for="password_confirm">Confirm Password:</label>
-                <input type="password" name="password_confirm" id="password_confirm" />
-            </div>
-
+				<?php if (isset($validUsername) && !$validUsername) {
+					echo "<span class='error'>username is not valid</span>";
+				} elseif (isset($usernameFree) && !$usernameFree) {
+					echo "<span class='error'>username already in use</span>";
+				} ?>
+      </div>
+      <div class="form-element">
+        <label for="name">Name:</label>
+        <input type="text" name="name" id="name" />
+				<input type="checkbox" name="check[]" value='name'/>
+				<?php if (isset($validName) && !$validName) {
+					echo "<span class='error'>name is not valid</span>";
+				} ?>
+     	</div>
+      <div class="form-element">
+        <label for="email">Email:</label>
+        <input type="email" name="email" id="email" />
+				<input type="checkbox" name="check[]" value='email'/>
+				<?php if (isset($validEmail) && !$validEmail) {
+					echo "<span class='error'>email is not valid</span>";
+				} elseif (isset($emailFree) && !$emailFree) {
+					echo "<span class='error'>email already in use</span>";
+				} ?>
+     	</div>
+      <div class="form-element">
+        <label for="password">Password:</label>
+        <input type="password" name="password" id="password" />
+				<input type="checkbox" name="check[]" value='password'/>
+				<?php if (isset($validPassword) && !$validPassword) {
+					echo "<span class='error'>" . $passErr . "</span>";
+				} ?>
+     	</div>
+      <div class="form-element">
+        <label for="password_confirm">Confirm Password:</label>
+        <input type="password" name="password_confirm" id="password_confirm" />
+				<?php if (isset($passwordMatchesConfirm) && !$passwordMatchesConfirm) {
+					echo "<span class='error'>Could not confirm password</span>";
+				} ?>
+      </div>
 			<div class="form-buttons">
-                <input type="submit" value="Update Account!" />
-                <input type="reset" />
-            </div>
-        </form>
-    </div>
+        <input type="submit" value="Update Account!" />
+        <input type="reset" />
+      </div>
+    </form>
+  </div>
 </body>
 </html>
